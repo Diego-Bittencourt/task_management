00502771cfe44a8581d69afc1b75d77a7a1b8887
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-09-15 13:32:52 +0900

    style the message data

--- src/components/messages/MessageItem.vue
+++ src/components/messages/MessageItem.vue
@@ -1,8 +1,9 @@
 <template>
   <li>
     <base-card>
-      <h2>From: {{ sender }}</h2>
+      <div class="msgdata"><h2>From: {{ sender }}</h2>
       <h2>Date: {{ date }}</h2>
+      </div>
       <p>{{ msgcontent }}</p>
     </base-card>
   </li>
@@ -13,3 +14,12 @@ export default {
   props: ["sender", "msgcontent", "date"],
 };
 </script>
+
+<style scoped>
+.msgdata {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin-bottom: 2rem;
+}
+</style>
