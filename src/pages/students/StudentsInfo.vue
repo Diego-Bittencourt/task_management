aa0801151b368beb110bb9f5f5f10769425ac012
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-10-03 23:51:57 +0900

    style the buttons on the students section

--- src/pages/students/StudentsInfo.vue
+++ src/pages/students/StudentsInfo.vue
@@ -1,9 +1,19 @@
 <template>
     <section>
         <base-card>
+        <div class="btnwrapper">
         <base-button to="/students/list" link>Students List</base-button>
         <base-button to="/students/addstudent" link>Add Students</base-button>
+        </div>
         </base-card>
         <router-view></router-view>
     </section>
 </template>
+
+<style scoped>
+.btnwrapper {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+}
+</style>
