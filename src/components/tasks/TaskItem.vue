3d85ca564070f55abf1343c39d3cfa76f4594bec
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-10-24 11:32:37 +0900

    add comment section input

--- src/components/tasks/TaskItem.vue
+++ src/components/tasks/TaskItem.vue
@@ -7,9 +7,7 @@
         <div class="column">
           <p>Sent by: {{ tasksender }}</p>
           <p>
-            <base-badge :format="tasktags">{{
-              tasktagsUP
-            }}</base-badge>
+            <base-badge :format="tasktags">{{ tasktagsUP }}</base-badge>
           </p>
         </div>
         <div class="column">
@@ -24,8 +22,28 @@
         </div>
       </div>
       <p class="taskdate">Date: {{ taskdate }}</p>
-      <p class="showmsg" @click="toggleMsg"><span v-if="!isMsgvisible">See Comments</span><span v-else>Hide Comments</span></p>
-      <div class="msgwrapper"></div>
+      <p class="showmsg" @click="toggleMsg">
+        <span v-if="!isMsgvisible">🔽 See Comments 🔽</span>
+        <span v-else>🔼 Hide Comments 🔼</span>
+      </p>
+      <span v-if="isMsgvisible">
+      <div
+        class="msgwrapper"
+        v-for="comment in taskcomments"
+        :key="comment.name"
+        :name="comment.name"
+        :comment="comment.comment"
+        :date="comment.date"
+      >
+        <p class="taskcomment"><span class="bold">{{comment.name}}:</span> {{comment.comment}}</p>
+        <p class="taskdate">Commented on: {{comment.date}}</p>
+      </div>
+      <div class="sendcomment">
+        <label for="addtaskcomment">Write a comment</label>
+        <textarea id="addtaskcomment" class="commentinput" rows="3"></textarea>
+        <base-button>Comment</base-button>
+        </div>
+      </span>
     </base-card>
   </li>
 </template>
@@ -40,23 +58,29 @@ export default {
     "tasktags",
     "taskId",
   ],
-  emits: ['getTasks'],
+  emits: ["getTasks"],
   data() {
     return {
       changeStatus: false,
       dialogTitle: "Confirm change status to " + this.setStatus,
       setStatus: "",
       error: null,
-      isMsgvisible: false
+      isMsgvisible: false,
     };
   },
   computed: {
+    taskcomments() {
+      return [
+        { name: "Joao", comment: "tralala", date: "01/02/2022" },
+        { name: "layana", comment: "tchulelele", date: "02/04/2022" },
+      ];
+    },
     taskstatusUP() {
-     return this.taskstatus.toUpperCase();
+      return this.taskstatus.toUpperCase();
     },
     tasktagsUP() {
       return this.tasktags.toUpperCase();
-    }
+    },
   },
   methods: {
     toggleMsg() {
@@ -65,8 +89,8 @@ export default {
     async changeTaskStatus(status) {
       let task = {
         id: this.taskId,
-        status: status
-      } //create payload object
+        status: status,
+      }; //create payload object
 
       try {
         await this.$store.dispatch("tasks/changeStatus", task);
@@ -74,13 +98,54 @@ export default {
         this.error = error.message || "Something went wrong";
       }
 
-      this.$emit('getTasks');
-    }
+      this.$emit("getTasks");
+    },
   },
 };
 </script>
 
 <style scoped>
+.bold {
+  font-weight: bold;
+}
+
+.commentinput:focus {
+ outline: none;
+ border-color: lightseagreen;
+ background-color: rgba(114, 103, 184, 0.265);
+}
+
+.commentinput {
+  resize: none;
+  width: 100%;
+
+}
+
+.sendcomment {
+  padding-top: 2rem;
+  margin-top: 2rem;
+  border-top: 2px solid #ccc;
+}
+
+.sendcomment * {
+  display: block;
+  margin-top: 0.5rem;
+}
+
+.taskcomment {
+  text-align: left;
+}
+
+label {
+  font-weight: bold;
+  text-align: left;
+}
+
+.msgwrapper {
+  border-top: 1px solid #ccc;
+  margin-top: 2rem;
+}
+
 .taskdata {
   display: flex;
   flex-direction: row;
